9d98ec7abbcb3b9ed8bcf2451d1850ea2e648901
Madison <56138832+madison12818@users.noreply.github.com>
2020-11-16 16:14:10 -0500

    Update Peer.py
    A class from which you will instantiate four objects: AS_1, AS_2, RS, and Client

--- Peer.py
+++ Peer.py
@@ -1,1 +1,21 @@
+'''
 
+Peer class has the following components:
+1. Resource data structure (L) : a JSON format file: { “C1”: “discount 20%”
+                                                       “C2”: “Credit Score 780”
+                                                       “C3”: “Alex.kent.edu”,
+                                                       “C4”: “ID81023456”
+                                                     }
+2. Policy data structure: a JSON format file { “C1”:[[“C2”, “C3”],
+                                               “C2”: [True],
+                                               “C3”: [“C4”],
+                                               “C4”: “TRUE”
+                                             }
+
+3. send a message method: call the message class and create a message to other peers
+4. Receive a message method:
+5. Resolution Resolver: an algorithm that will be invoked when a message m is received. It extracts the
+   requested credential (C) from the message, check the policy and send new messages to request all the item
+   in the policy from different parties. See the example in Resolution_Resolver.py
+
+'''
