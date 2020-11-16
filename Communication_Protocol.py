d4f1cd57f94fb4a6115a3dc85dbe0f2e6fa0e111
Madison <56138832+madison12818@users.noreply.github.com>
2020-11-16 16:09:41 -0500

    Update Communication_Protocol.py
    An algorithm that manages the sending and receiving of messages between peers.

--- Communication_Protocol.py
+++ Communication_Protocol.py
@@ -1,1 +1,27 @@
+'''
 
+Participate In OAuth () {
+  while willing to participate do
+    if the incoming message queue is empty then
+      Wait a short period of time for new messages
+    if there is a new message in the incoming message queue then
+      Choose such a message m and remove it from the queue
+      Record m’s receipt time as the current time
+      ProcessMessage(m)}
+/* message handler */
+
+ProcessMessage (m) {
+/* Mreceived and Msent store received and sent messages respectively */
+Mreceived = Mreceived ∪ {m}
+If m is an offer, add m to the local knowledge base L ( resources_vault)
+Apply the resolution resolver algorithm with parameters Mreceived, Msent, and L,
+which returns a list of messages M
+/* send the messages to their intended recipients */
+  if M is not empty then
+     for every message k in M do
+        Send k to its specified recipient
+        Record k’s sending time as the current time
+        Msent = Msent ∪ {k}
+}
+
+'''
